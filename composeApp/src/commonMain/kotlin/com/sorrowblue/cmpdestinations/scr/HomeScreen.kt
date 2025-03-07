79a07f10d309f8a13a9a622dbdc215c5e7841f63
Sorrow Blue <sorrowblue.dev@gmail.com>
2025-03-07 17:01:05 +0900

    #4 Fixed Datekt point

--- composeApp/src/commonMain/kotlin/com/sorrowblue/cmpdestinations/scr/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sorrowblue/cmpdestinations/scr/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.sorrowblue.cmpdestinations.scr
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import com.sorrowblue.cmpdestinations.annotation.Destination
 import com.sorrowblue.cmpdestinations.annotation.DestinationInGraph
 import com.sorrowblue.cmpdestinations.annotation.NavGraph
@@ -14,7 +15,7 @@ data class SeriType(
 
 enum class Types {
     Nomal,
-    Hard
+    Hard,
 }
 
 @Serializable
@@ -61,49 +62,63 @@ data class Home2(
 @Composable
 @Destination<Home2>
 fun HomeScreen2() {
-
+    LaunchedEffect(Unit) {
+        println("HomeScreen2")
+    }
 }
 
 @Composable
 @Destination<Home>
 fun HomeScreen() {
-
+    LaunchedEffect(Unit) {
+        println("HomeScreen")
+    }
 }
 
 @Serializable
 data object A1
+
 @Serializable
 data object A2
+
 @Serializable
 data object A3
+
 @Serializable
 data object A4
+
 @Serializable
 data object A5
-
 
 @Composable
 @Destination<A1>
 fun AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1Screen() = Unit
+
 @Composable
 @Destination<A2>
 fun AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1Scree2() = Unit
+
 @Composable
 @Destination<A3>
 fun AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1Scree3() = Unit
+
 @Composable
 @Destination<A4>
 fun AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1Scree4() = Unit
+
 @Composable
 @Destination<A5>
 fun AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1Scree5() = Unit
+
 @Serializable
 data object Second
 
 @Composable
 @Destination<Second>
 fun SecondScreen(route: Second) {
-
+    LaunchedEffect(Unit) {
+        println("SecondScreen(route: $route)")
+    }
 }
 
 @NavGraph(startDestination = Home::class)
